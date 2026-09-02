308ebd77e07cda3a6bb5884ff2d01abcadcf380c
Andres Manuel Amezcua Alcantar <aamezcua28@ucol.mx>
2026-09-02 13:04:38 -0600

    Primer proyecto

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("======================================\n");
+    printf("            HOLA, GITHUB\n");
+    printf("======================================\n");
+    printf("");
+
+
+
+    return 0;
+}
